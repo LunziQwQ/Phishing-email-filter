7e0d4bdd589597fb03322cf7a2eed9ad939533fe
Kylin-Jiang <ziqi.jiang@xtalpi.com>
2019-03-25 19:11:49 +0800

    refactor checker; create new test for UrlChecker;

--- checker/checker.py
+++ checker/checker.py
@@ -1,7 +1,5 @@
 from checker.url_checker import UrlChecker
 from utils.system_info import is_connected
-from data.phish_tank_db import PhishTankDB
-from data.unusual_char_db import UnusualCharDB
 
 
 class Checker(object):
@@ -10,53 +8,14 @@ class Checker(object):
         self.phish_tank_db = None
         self.unusual_db = None
 
-    def init_phish_tank_db(self):
-        self.phish_tank_db = PhishTankDB()
+    def check(self, email_info, check_list):
+        is_online = is_connected()
+        checkers = [
+            UrlChecker(email_info, check_list, is_online),
+        ]
+        result = {}
 
-    def init_unusual_db(self):
-        self.unusual_db = UnusualCharDB()
-
-    def check(self, email_info):
-        result = {
-            "url": {
-                "count": len(email_info.links),
-                "have_ip": 0,
-                "netloc_too_long": 0,
-                "low_pr": 0,
-                "have_unusual": 0,
-                "in_phish_tank": 0,
-                "create_less_3_month": 0
-            }
-        }
-
-        # check url
-        for link in email_info.links:
-
-            # 检查链接中包含IP
-            if UrlChecker.have_ip(link):
-                result["url"]["have_ip"] += 1
-
-            # 检查链接主域名过长
-            if UrlChecker.netloc_too_long(link):
-                result["url"]["netloc_too_long"] += 1
-
-            # 检查是否存在特殊字符
-            if self.unusual_db and UrlChecker.have_unusual_char(link, self.unusual_db):
-                result["url"]["have_unusual"] += 1
-
-            # 检查是否存在于PhishTank数据库
-            if self.phish_tank_db and UrlChecker.in_phish_tank(link, self.phish_tank_db):
-                result["url"]["in_phish_tank"] += 1
-
-            # 依赖网络接口的检查
-            if is_connected():
-
-                # 检查PageRank过低
-                if UrlChecker.low_pr(link):
-                    result["url"]["low_pr"] += 1
-
-                # 检查创建时间是否少于3个月
-                if UrlChecker.create_less_3_month(link):
-                    result["url"]["create_less_3_month"] += 1
+        for checker in checkers:
+            result.update(checker.check())
 
         return result
--- checker/url_checker.py
+++ checker/url_checker.py
@@ -2,20 +2,75 @@ import datetime
 import urllib3
 import re
 
+from data.phish_tank_db import PhishTankDB
+from data.unusual_char_db import UnusualCharDB
 from utils.sogou_rank_query import get_sogou_rank
 from utils.whois_query import get_create_time
 
 ip_regex = r'(?:[0-9]{1,3}\.){3}[0-9]{1,3}'
+full_check_list = ["have_ip",
+                   "netloc_too_long",
+                   "low_pr",
+                   "have_unusual",
+                   "in_phish_tank",
+                   "create_less_3_month"]
 
 
 class UrlChecker:
+    phish_tank_db = None
+    unusual_db = None
+
+    def __init__(self, eml_info, check_list, is_connected):
+        self.eml_info = eml_info
+        self.check_result = {
+            "url": {
+                "count": len(self.eml_info.urls),
+                "have_ip": 0,
+                "netloc_too_long": 0,
+                "low_pr": 0,
+                "have_unusual": 0,
+                "in_phish_tank": 0,
+                "create_less_3_month": 0
+            }
+        }
+        self.check_list = check_list
+        self.is_connected = is_connected
+
+    def check(self):
+        for url in self.eml_info.urls:
+            if "have_ip" in self.check_list:
+                self.check_result["url"]["have_ip"] += 1 if UrlChecker.have_ip(url) else 0
+            if "netloc_too_long" in self.check_list:
+                self.check_result["url"]["netloc_too_long"] += 1 if UrlChecker.netloc_too_long(url) else 0
+            if "have_unusual" in self.check_list:
+                self.check_result["url"]["have_unusual"] += 1 if UrlChecker.have_unusual_char(url) else 0
+            if "in_phish_tank" in self.check_list:
+                self.check_result["url"]["in_phish_tank"] += 1 if UrlChecker.in_phish_tank(url) else 0
+
+            if self.is_connected:
+
+                if "create_less_3_month" in self.check_list:
+                    self.check_result["url"]["create_less_3_month"] += 1 if UrlChecker.create_less_3_month(url) else 0
+                if "low_pr" in self.check_list:
+                    self.check_result["url"]["low_pr"] += 1 if UrlChecker.low_pr(url) else 0
+
+        return self.check_result
+
+    @classmethod
+    def init_phish_tank_db(cls):
+        cls.phish_tank_db = PhishTankDB()
+
+    @classmethod
+    def init_unusual_db(cls):
+        cls.unusual_db = UnusualCharDB()
+
     @staticmethod
     def have_ip(url):
         return len(re.findall(ip_regex, url)) > 0
 
     @staticmethod
     def netloc_too_long(url):
-        return len(urllib3.util.parse_url(url).netloc) > 24
+        return len(urllib3.util.parse_url(url).netloc) > 30
 
     @staticmethod
     def low_pr(url):
@@ -23,13 +78,18 @@ class UrlChecker:
             return True
         return False
 
-    @staticmethod
-    def in_phish_tank(url, db):
-        return db.is_phish_url(url)
+    @classmethod
+    def in_phish_tank(cls, url):
+        if not cls.phish_tank_db:
+            cls.init_phish_tank_db()
 
-    @staticmethod
-    def have_unusual_char(url, db):
-        return db.have_unusual(url)
+        return cls.phish_tank_db.is_phish_url(url)
+
+    @classmethod
+    def have_unusual_char(cls, url):
+        if not cls.unusual_db:
+            cls.init_unusual_db()
+        return cls.unusual_db.have_unusual(url)
 
     @staticmethod
     def create_less_3_month(url):
