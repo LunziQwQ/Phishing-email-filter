74e5de4ca1e8a2ca14ceac56776b85972a52fc4c
C2tr <978807954@qq.com>
2019-03-31 00:50:13 +0800

    add a main_contoller and finish a ui demo

--- ui/controller/main_controller.py
+++ ui/controller/main_controller.py
@@ -1,5 +1,8 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
-from ui.main import Ui_MainWindow
+from PyQt5.QtCore import QFile
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
+
+from reader.eml_reader import EmlReader
+from ui.view.main import Ui_MainWindow
 
 
 class MainController(QMainWindow, Ui_MainWindow):
@@ -7,3 +10,16 @@ class MainController(QMainWindow, Ui_MainWindow):
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+
+        self.open_eml.triggered.connect(self.read_eml)
+
+    def read_eml(self):
+        fname = QFileDialog.getOpenFileName(self, "Open File", "./", "Eml (*.eml)")
+        # 打开文件 返回一个字符串第一个是路径， 第二个是要打开文件的类型
+        # 如果用户主动关闭文件对话框，则返回值为空
+        if fname[0]:  # 判断路径非空
+            f = QFile(fname[0])  # 创建文件对象，不创建文件对象也不报错 也可以读文件和写文件
+            # open()会自动返回一个文件对象
+            reader = EmlReader(fname[0])
+            info = reader.read()
+            self.textEdit.setText(info.__str__())
--- ui/view/main.py
+++ ui/view/main.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/Users/lunzi/Projects/Python/phishing-email-filter/ui/main.ui'
+# Form implementation generated from reading ui file 'C:\Users\kiven\PycharmProjects\Phishing-email-filter\ui\view\main.ui'
 #
-# Created by: PyQt5 UI code generator 5.12
+# Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -16,9 +15,18 @@ class Ui_MainWindow(object):
         MainWindow.setUnifiedTitleAndToolBarOnMac(False)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(80, 130, 93, 28))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(390, 160, 72, 15))
+        self.label.setObjectName("label")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(210, 120, 531, 361))
+        self.textEdit.setObjectName("textEdit")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 28))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
@@ -26,9 +34,9 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-        self.action_eml = QtWidgets.QAction(MainWindow)
-        self.action_eml.setObjectName("action_eml")
-        self.menuFile.addAction(self.action_eml)
+        self.open_eml = QtWidgets.QAction(MainWindow)
+        self.open_eml.setObjectName("open_eml")
+        self.menuFile.addAction(self.open_eml)
         self.menubar.addAction(self.menuFile.menuAction())
 
         self.retranslateUi(MainWindow)
@@ -37,7 +45,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Phishing Email Filter"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.label.setText(_translate("MainWindow", "TextLabel"))
         self.menuFile.setTitle(_translate("MainWindow", "文件"))
-        self.action_eml.setText(_translate("MainWindow", "打开eml文件"))
-
+        self.open_eml.setText(_translate("MainWindow", "打开eml文件"))
 
