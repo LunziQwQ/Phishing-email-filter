537be99c6a2e0637f07545374035e3309f46e2d4
C2tr <978807954@qq.com>
2019-04-26 12:18:33 +0800

    main.ui 修改“Start”按钮 添加邮件列表显示

--- checker/checker.py
+++ checker/checker.py
@@ -4,6 +4,14 @@ from utils.system_info import is_connected
 
 class Checker(object):
 
+    detect_time = {
+        "plain": 2,
+        "url_basic": 0.3,
+        "html": 0.2,
+        "accessory": 0.1,
+        "url_advance": 2
+    }
+
     def __init__(self):
         self.phish_tank_db = None
         self.unusual_db = None
